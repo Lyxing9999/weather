24e1af1e8871446982ee8f0197c9da844db8367d
kaingbunly <kaingbunly9999@gmail.com>
2025-03-20 16:50:05 +0700

    Added copyright footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <router-view></router-view>
+  <Footer />
 </template>
-<script setup></script>
-<style scoped></style>
+<script setup>
+import Footer from "@/components/Footer.vue";
+</script>
+
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  position: relative;
+}
+footer {
+  margin-top: auto;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="copyright">
+    <p>&copy; 2025 Kaing Bunly. All Rights Reserved.</p>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 10px;
+  background-color: transparent;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 200px;
+  height: 50px;
+  box-sizing: border-box;
+}
+
+.copyright p {
+  margin: 0;
+  font-size: 12px;
+  color: #666;
+}
+</style>
